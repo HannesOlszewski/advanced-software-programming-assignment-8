91f982070d4d7ee2414926fc5c94bccae185402d
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2023-12-20 23:21:29 +0100

    feat: Refactor main function to handle directory input and error handling

--- file-system-analyzer/app/Main.hs
+++ file-system-analyzer/app/Main.hs
@@ -1,13 +1,25 @@
 module Main (main) where
 
 import Lib
+import System.Environment (getArgs)
+import System.IO (hPutStrLn, stderr)
+import System.Exit (exitWith, ExitCode(..))
+import Control.Exception (IOException, catch)
 
 main :: IO ()
 main = do
-    putStrLn "Enter a directory to analyze: "
-    directory <- getLine
-    stats <- analyzeDirectoryWithSimpleProcessor directory
-    putStrLn $ "Analysing directory: " ++ directory
-    putStrLn $ "Total size: " ++ prettyPrintSize (totalSize stats)
-    putStrLn $ "Total files: " ++ show (totalFiles stats)
-    putStrLn $ "File type counts: " ++ show (sortFileTypeCounts DESC (fileTypeCounts stats))
+    catch (do
+        args <- getArgs
+        let directory = head args
+        stats <- analyzeDirectoryWithSimpleProcessor directory
+        putStrLn $ "Analysing directory: " ++ directory
+        putStrLn $ "Total size: " ++ prettyPrintSize (totalSize stats)
+        putStrLn $ "Total files: " ++ show (totalFiles stats)
+        putStrLn $ "File type counts: " ++ show (sortFileTypeCounts DESC (fileTypeCounts stats))
+        )
+        (\e -> do
+            let err = show (e :: IOException)
+            hPutStrLn stderr ("Error (in catch): " ++ err)
+            exitWith (ExitFailure 1)
+        )
+
